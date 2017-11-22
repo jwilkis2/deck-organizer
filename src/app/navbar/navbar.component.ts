09fb651bd16a2d25a2f0dd3c6c556b895279442f
Jonathan Wilkison <jwilkis2@gmail.com>
2017-11-21 19:28:10 -0600

    global logout button and static chat box
    Will implement chat box functionality once i get the scryfall api working

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent {
+  user;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(public afAuth: AngularFireAuth, private router: Router) {
+    this.afAuth.auth.onAuthStateChanged(user => {
+      if (user) {
+        this.user = user;
+      } else {
+        this.user = undefined;
+        //this.router.navigate(['/login']);
+      }
+    });
   }
+
+  logout() {
+    this.afAuth.auth.signOut();
+    this.router.navigate(['/login']);
+  }
+
 
 }
